eab0dfde0c5b68b80a815f46fe5bc7aa63248aa3
shj1987 <huajieshao1987@gmail.com>
2018-05-09 06:15:16 -0500

    update new website

---
+++ Website/js/style.js
@@ -0,0 +1,9 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("demo").innerHTML = myObj.name;
+    }
+};
+xmlhttp.open("GET", "data/data.json", true);
+xmlhttp.send();
